aee7d807ac7a62b20eed40680089b34d5d06e4fd
jianhao.dai <jianhao.dai@fraudmetrix.cn>
2015-08-01 17:34:07 +0800

    config

--- sae.php
+++ sae.php
@@ -11,5 +11,5 @@ defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 defined('YII_PATH') or define('YII_PATH',dirname(__FILE__).'/framework/yii/framework');
 
 require_once(dirname(__FILE__).'/framework/redlib/red.php');
-$config = dirname(__FILE__).'/protected/config/sae.php';
+$config = dirname(__FILE__).'/protected/config/main.php';
 Yii::createSaeWebApplication($config)->run();
